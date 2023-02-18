da4e506762bbc09dc2d3fad1d5f3c3b7da06f60b
KatePetukh <katsiaryna.dudko@gmail.com>
2023-02-18 23:47:54 +0300

    HW2: fix for user extensions

--- niffler-e-2-e-kate-tests/src/test/java/niffler/jupiter/UserExtension.java
+++ niffler-e-2-e-kate-tests/src/test/java/niffler/jupiter/UserExtension.java
@@ -18,8 +18,8 @@ public class UserExtension implements
         AfterTestExecutionCallback,
         ParameterResolver {
 
-    public static final ExtensionContext.Namespace NAMESPACE
-            = ExtensionContext.Namespace.create(UserExtension.class);
+    public static final ExtensionContext.Namespace NAMESPACE =
+            ExtensionContext.Namespace.create(UserExtension.class);
 
     private static final Queue<UserModel> USER_MODEL_ADMIN_QUEUE = new ConcurrentLinkedQueue<>();
     private static final Queue<UserModel> USER_MODEL_COMMON_QUEUE = new ConcurrentLinkedQueue<>();
@@ -31,43 +31,47 @@ public class UserExtension implements
     }
 
     @Override
-    public void beforeTestExecution(ExtensionContext context) throws Exception {
+    public void beforeTestExecution(ExtensionContext context) {
         String id = getTestId(context);
 
         List<Parameter> parameters = new LinkedList<>(Arrays.asList(context.getRequiredTestMethod().getParameters()));
-        List<User.UserType> desiredUserTypes = new LinkedList<>();
+        Map<String, User.UserType> desiredUserTypes = new HashMap<>();
         for (Parameter parameter : parameters) {
             if (parameter.isAnnotationPresent(User.class)) {
-                desiredUserTypes.add(parameter.getAnnotation(User.class).userType());
+                desiredUserTypes.put(parameter.getName(), parameter.getAnnotation(User.class).userType());
             }
         }
 
-        Map<User.UserType, UserModel> users = new LinkedHashMap<>();
-        for (User.UserType desiredUserType : desiredUserTypes) {
+        Map<String, UserModel> users = new LinkedHashMap<>();
+        for (Map.Entry<String, User.UserType> userType : desiredUserTypes.entrySet()) {
             UserModel user = null;
             while (user == null) {
-                if (desiredUserType == User.UserType.ADMIN) {
+                if (userType.getValue() == User.UserType.ADMIN) {
                     user = USER_MODEL_ADMIN_QUEUE.poll();
                 } else {
                     user = USER_MODEL_COMMON_QUEUE.poll();
                 }
             }
             Objects.requireNonNull(user);
-            users.put(desiredUserType, user);
+            users.put(userType.getKey(), user);
         }
         context.getStore(NAMESPACE).put(id, users);
     }
 
     @Override
-    public void afterTestExecution(ExtensionContext context) throws Exception {
+    public void afterTestExecution(ExtensionContext context) {
         String id = getTestId(context);
-        Map<User.UserType, UserModel> users = context.getStore(NAMESPACE).get(id, Map.class);
+        Map<String, UserModel> users = context.getStore(NAMESPACE).get(id, Map.class);
 
-        for (Map.Entry<User.UserType, UserModel> user : users.entrySet()) {
-            if (user.getKey().equals(User.UserType.ADMIN)) {
-                USER_MODEL_ADMIN_QUEUE.add(user.getValue());
-            } else {
-                USER_MODEL_COMMON_QUEUE.add(user.getValue());
+        List<Parameter> parameters = new LinkedList<>(Arrays.asList(context.getRequiredTestMethod().getParameters()));
+        for (Parameter parameter : parameters) {
+            if (parameter.isAnnotationPresent(User.class)) {
+                UserModel user = users.get(parameter.getName());
+                if (parameter.getAnnotation(User.class).userType().equals(User.UserType.ADMIN)) {
+                    USER_MODEL_ADMIN_QUEUE.add(user);
+                } else {
+                    USER_MODEL_COMMON_QUEUE.add(user);
+                }
             }
         }
     }
@@ -87,6 +91,6 @@ public class UserExtension implements
     public UserModel resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
         String id = getTestId(extensionContext);
         return (UserModel) extensionContext.getStore(NAMESPACE).get(id, Map.class)
-                .get(parameterContext.getParameter().getDeclaredAnnotation(User.class).userType());
+                .get(parameterContext.getParameter().getName());
     }
 }
