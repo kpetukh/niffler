3f3929a07f35445cc62163e93dc94fd230011f40
KatePetukh <katsiaryna.dudko@gmail.com>
2023-02-18 21:28:14 +0300

    Revert "HW2: task 3"
    This reverts commit dbf61da8

--- niffler-e-2-e-tests/src/test/java/niffler/jupiter/extension/UsersExtension.java
+++ niffler-e-2-e-tests/src/test/java/niffler/jupiter/extension/UsersExtension.java
@@ -10,8 +10,10 @@ import org.junit.jupiter.api.extension.ParameterContext;
 import org.junit.jupiter.api.extension.ParameterResolutionException;
 import org.junit.jupiter.api.extension.ParameterResolver;
 
-import java.lang.reflect.Parameter;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class UsersExtension implements
@@ -34,42 +36,34 @@ public class UsersExtension implements
     @Override
     public void beforeTestExecution(ExtensionContext context) throws Exception {
         String id = getTestId(context);
+        User.UserType desiredUserType = Arrays.stream(context.getRequiredTestMethod()
+                        .getParameters())
+                .filter(p -> p.isAnnotationPresent(User.class))
+                .map(p -> p.getAnnotation(User.class))
+                .findFirst()
+                .orElseThrow()
+                .userType();
 
-        List<Parameter> parameters = new LinkedList<>(Arrays.asList(context.getRequiredTestMethod().getParameters()));
-        List<User.UserType> desiredUserTypes = new LinkedList<>();
-        for (Parameter parameter : parameters) {
-            if (parameter.isAnnotationPresent(User.class)) {
-                desiredUserTypes.add(parameter.getAnnotation(User.class).userType());
+        UserModel user = null;
+        while (user == null) {
+            if (desiredUserType == User.UserType.ADMIN) {
+                user = USER_MODEL_ADMIN_QUEUE.poll();
+            } else {
+                user = USER_MODEL_COMMON_QUEUE.poll();
             }
         }
-
-        Map<User.UserType, UserModel> users = new LinkedHashMap<>();
-        for (User.UserType desiredUserType : desiredUserTypes) {
-            UserModel user = null;
-            while (user == null) {
-                if (desiredUserType == User.UserType.ADMIN) {
-                    user = USER_MODEL_ADMIN_QUEUE.poll();
-                } else {
-                    user = USER_MODEL_COMMON_QUEUE.poll();
-                }
-            }
-            Objects.requireNonNull(user);
-            users.put(desiredUserType, user);
-        }
-        context.getStore(NAMESPACE).put(id, users);
+        Objects.requireNonNull(user);
+        context.getStore(NAMESPACE).put(id, Map.of(desiredUserType, user));
     }
 
     @Override
     public void afterTestExecution(ExtensionContext context) throws Exception {
         String id = getTestId(context);
-        Map<User.UserType, UserModel> users = context.getStore(NAMESPACE).get(id, Map.class);
-
-        for (Map.Entry<User.UserType, UserModel> user : users.entrySet()) {
-            if (user.getKey().equals(User.UserType.ADMIN)) {
-                USER_MODEL_ADMIN_QUEUE.add(user.getValue());
-            } else {
-                USER_MODEL_COMMON_QUEUE.add(user.getValue());
-            }
+        Map<User.UserType, UserModel> map = context.getStore(NAMESPACE).get(id, Map.class);
+        if (map.containsKey(User.UserType.ADMIN)) {
+            USER_MODEL_ADMIN_QUEUE.add(map.get(User.UserType.ADMIN));
+        } else {
+            USER_MODEL_COMMON_QUEUE.add(map.get(User.UserType.COMMON));
         }
     }
 
@@ -89,6 +83,8 @@ public class UsersExtension implements
     public UserModel resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
         String id = getTestId(extensionContext);
         return (UserModel) extensionContext.getStore(NAMESPACE).get(id, Map.class)
-                .get(parameterContext.getParameter().getDeclaredAnnotation(User.class).userType());
+                .values()
+                .iterator()
+                .next();
     }
 }
--- niffler-e-2-e-tests/src/test/java/niffler/test/NifflerLoginTest.java
+++ niffler-e-2-e-tests/src/test/java/niffler/test/NifflerLoginTest.java
@@ -4,25 +4,25 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 import io.qameta.allure.Allure;
 import io.qameta.allure.AllureId;
-import niffler.jupiter.extension.ScreenshotExtension;
-import niffler.jupiter.annotation.User;
-import niffler.jupiter.extension.UsersExtension;
+import niffler.jupiter.ScreenshotExtension;
+import niffler.jupiter.User;
+import niffler.jupiter.UsersExtension;
 import niffler.model.UserModel;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import static com.codeborne.selenide.Selenide.$;
-import static niffler.jupiter.annotation.User.UserType.ADMIN;
+import static niffler.jupiter.User.UserType.ADMIN;
 
 @ExtendWith({ScreenshotExtension.class, UsersExtension.class})
-public class NifflerLoginTest extends BaseTest {
+public class NifflerLoginTest {
 
     @AllureId("1")
     @Test
     void mainPageShouldBeDisplayedAfterSuccessLogin(@User(userType = ADMIN) UserModel user) {
         System.out.println("#### Test 1 " + user.toString());
         Allure.step("Check login", () -> {
-            Selenide.open(CFG.frontUrl());
+            Selenide.open("http://127.0.0.1:3000/");
             $("a[href*='redirect']").click();
             $("input[name='username']").setValue("dima");
             $("input[name='password']").setValue("12345");
@@ -33,7 +33,6 @@ public class NifflerLoginTest extends BaseTest {
     }
 
     @AllureId("2")
-
     @Test
     void mainPageShouldBeDisplayedAfterSuccessLogin0(@User(userType = ADMIN) UserModel user) {
         System.out.println("#### Test 2 " + user.toString());
